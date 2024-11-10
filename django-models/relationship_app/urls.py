b2300f0102a32e77735d60314ce95a10d2729123
abderrahmane lafifi <lafifiabd@gmail.com>
2024-11-10 22:53:52 +0100

    views and log in

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -11,6 +11,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
+
+    path('book/', views.list_books, name'list_books'),
+    path('books/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
