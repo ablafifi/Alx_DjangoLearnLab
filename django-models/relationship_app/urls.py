80ba6a268cdfe1266c17de41a3c6bf7d5d0c4238
abderrahmane lafifi <lafifiabd@gmail.com>
2024-11-10 23:08:39 +0100

    urls

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-]
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from . import views
-
-urlpatterns = [
-
-    path('book/', views.list_books, name'list_books'),
-    path('books/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
