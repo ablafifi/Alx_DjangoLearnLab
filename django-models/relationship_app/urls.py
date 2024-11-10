d1a07e886f0779f8fbdefda2afc77be9170ed661
abderrahmane lafifi <lafifiabd@gmail.com>
2024-11-10 22:03:34 +0100

    log in log out

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
